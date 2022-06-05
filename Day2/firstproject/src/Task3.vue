c0a778546bf186d6d1ab24c058383a2519f2ce7b
abdelrahmangbr <abdelrahman.gbr.os@gmail.com>
2022-06-05 14:28:50 +0200

    demos

---
+++ Day2/firstproject/src/Task3.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="container">
+    <div class="row">
+        <div class="col-12 py-2 my-2">
+            <HeaderComponent/>
+        </div>
+    </div>
+    <div class="row my-2">
+        <div class="col-4"> 
+            <LinksComponent/>
+        </div>
+    </div>
+        <div class="row">
+        <div class="col-12">
+            <FooterComponent/>
+        </div>
+    </div>
+
+</div>
+</template>
+
+<script>
+import HeaderComponent from "./components/HeaderComponent.vue";
+import FooterComponent from "./components/FooterComponent.vue";
+import LinksComponent from "./components/LinksComponent.vue";
+export default {
+    components: { HeaderComponent, FooterComponent, LinksComponent }
+}
+</script>
