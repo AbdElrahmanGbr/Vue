bfb2d3482e81abb2a6f3c02b92438b60f771baf5
abdelrahmangbr <abdelrahman.gbr.os@gmail.com>
2022-06-09 13:05:03 +0200

    wraping lab 3

---
+++ Day3/firstproject/src/components/ContactComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Contact</h1>
+    <div>
+      <span class="h5">Phone Number:</span>
+      <p>01030885343</p>
+    </div>
+    <div>
+      <span class="h5">Email:</span>
+      <p>abdelrahman.gbr.os@gmail.com</p>
+    </div>
+</div>  
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- Day3/firstproject/src/main.js
+++ Day3/firstproject/src/main.js
@@ -1,14 +1,3 @@
-// import { createApp } from 'vue'
-
-// import App from './App.vue'
-// import Main from './Main.vue'
-// import Modal from './Modal.vue'
-// import Task3 from './Task3.vue'
-
-// createApp(Main).mount('#app')
-// // createApp(Modal).mount('#app')
-// // createApp(Task3).mount('#app')
-
 import { createApp } from 'vue'
 // import App from './App.vue'
 // import WrapperComponent from './WrapperComponent.vue'
@@ -17,5 +6,3 @@ import router from './routers/index'
 
 // createApp(App).mount('#app')
 createApp(HeaderComponent).use(router).mount('#app')
-
-
