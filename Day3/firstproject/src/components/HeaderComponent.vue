981594893be55a04952057372918b34b22e79912
abdelrahmangbr <abdelrahman.gbr.os@gmail.com>
2022-06-09 10:24:55 +0200

    starting with main components

--- Day3/firstproject/src/components/HeaderComponent.vue
+++ Day3/firstproject/src/components/HeaderComponent.vue
@@ -1,42 +1,39 @@
 <template>
-  <header class="p-3 bg-dark text-white">
-    <div class="container">
-      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
-        </a>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-2 container">
+      <div class="container-fluid">
+        <router-link class="navbar-brand" to="/"
+          ><i class="fa-solid fa-house-user"></i>
+        </router-link>
 
-        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">About</a></li>
-        </ul>
-
-        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-          <input type="search" class="form-control form-control-dark text-white bg-dark" placeholder="Search..." aria-label="Search">
-        </form>
-
-        <div class="text-end">
-          <button type="button" class="btn btn-outline-light me-2">Login</button>
-          <button type="button" class="btn btn-warning">Sign-up</button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item ms-4">
+              <router-link to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="mx-4" to="/first">First Routed</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/second">Second Routed</router-link>
+            </li>
+          </ul>
+          <form class="d-flex">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button class="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
         </div>
       </div>
-    </div>
-  </header>
-    
+    </nav>
+    <router-view></router-view>
+  </div>
 </template>
 
-<script>
-export default {
-    data() {
-        return{}
-    }
-}
-
-</script>
-
-<style scoped>
-
-</style>
+<script></script>
--- Day3/firstproject/src/main.js
+++ Day3/firstproject/src/main.js
@@ -1,12 +1,21 @@
-import { createApp } from 'vue'
+// import { createApp } from 'vue'
 
-import App from './App.vue'
-import Main from './Main.vue'
-import Modal from './Modal.vue'
-import Task3 from './Task3.vue'
+// import App from './App.vue'
+// import Main from './Main.vue'
+// import Modal from './Modal.vue'
+// import Task3 from './Task3.vue'
 
 // createApp(Main).mount('#app')
-// createApp(Modal).mount('#app')
-// createApp(Task3).mount('#app')
+// // createApp(Modal).mount('#app')
+// // createApp(Task3).mount('#app')
+
+import { createApp } from 'vue'
+// import App from './App.vue'
+// import WrapperComponent from './WrapperComponent.vue'
+import HeaderComponent from './components/HeaderComponent.vue'
+import router from './routers/index'
+
+// createApp(App).mount('#app')
+createApp(HeaderComponent).use(router).mount('#app')
 
 
